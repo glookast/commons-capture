f86647346eb970c1e129e27f3fa02ef52d1167e6
Tiago Pereira <tiago.pereira@glookast.com>
2018-11-20 13:57:09 +0000

    Added NamingRule. Updated developers list on build.gradle.

---
+++ src/main/java/com/glookast/commons/capture/NamingRule.java
@@ -0,0 +1,19 @@
+package com.glookast.commons.capture;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, defaultImpl = NamingRule.class)
+public class NamingRule
+{
+    protected Integer id;
+    protected String rule;
+
+}
